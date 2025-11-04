9556dc362c977274d229b545383bebf137906a66
AJAYK-1 <ajaykumartp10@gmail.com>
2025-11-04 20:49:56 +0530

    "Fullstack docker compose"

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,23 @@
 const express = require('express')
 const app = express()
 
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+
+const dotenv = require('dotenv')
+dotenv.config()
+
+const DBConnect = require('./Middleware/DBConnect')
+DBConnect()
+
+app.get('/connect', (req, res) => {
+    try {
+        console.log('Request recieved...');
+        res.status(200).json({ message: 'This is from backend...' })
+    } catch (error) {
+        console.error(error.message);
+    }
+})
+
 app.listen(5000, () => console.log('Server running at PORT: 5000'))
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,27 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [value, setValue] = useState('')
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_HOST_URL}connect`)
+      .then(res => {
+        if (!res.ok)
+          throw new Error("Response failed...")
+        return res.json()
+      })
+      .then(data => setValue(data.message))
+      .catch(err => console.error(err.message))
+  }, [])
+  console.log(value);
 
   return (
     <>
       <h1> Learn GitHub and DevOps </h1>
       <h2> Home Page </h2>
+      <h3> {value} </h3>
     </>
   )
 }
